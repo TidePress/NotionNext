0dfa5ef026f929c75fd04abc47643b7050016305
TidePress <144722416+TidePress@users.noreply.github.com>
2023-09-12 12:42:15 +0100

    Update Announcement.js

--- themes/fukasawa/components/Announcement.js
+++ themes/fukasawa/components/Announcement.js
@@ -8,7 +8,7 @@ const Announcement = ({ post, className }) => {
   if (post?.blockMap) {
     return <div className={className}>
         <section id='announcement-wrapper' className="dark:text-gray-300 text-left ">
-      //rounded-xl px-2 py-4 rounded-xl lg:p-6 p-4
+      /* rounded-xl px-2 py-4 rounded-xl lg:p-6 p-4 */
             <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div>
             {post && (<div id="announcement-content" className='texe-left'>
             <NotionPage post={post} />
