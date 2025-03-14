27f684a6539ac2bc30de1c678a922807f6dfe151
TidePress <144722416+TidePress@users.noreply.github.com>
2025-03-14 20:36:59 +0000

    Update Announcement.js

--- themes/fukasawa/components/Announcement.js
+++ themes/fukasawa/components/Announcement.js
@@ -7,7 +7,7 @@ const Announcement = ({ post, className }) => {
   const { locale } = useGlobal()
   if (post?.blockMap) {
     return <div className={className}>
-        <section id='announcement-wrapper' className="dark:text-gray-300 rounded-xl px-2 py-4">
+        <section id='announcement-wrapper' className="dark:text-gray-300 text-left">
             <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div>
             {post && (<div id="announcement-content">
             <NotionPage post={post} className='text-center ' />
