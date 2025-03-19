4a21b9f8e02343c1976e5e6b01ab0ec94e7abbdc
TidePress <144722416+TidePress@users.noreply.github.com>
2025-03-19 18:08:19 +0000

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Tide Press', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Pursuing the noble and endless answer', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tidepress.ink', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Tide Press', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '', // blog favicon 配置, 默认使用 /favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
