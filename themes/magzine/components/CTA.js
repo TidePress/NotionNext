a40d6162e3865f845c852b2d1bf42bd0285af8ad
TidePress <144722416+TidePress@users.noreply.github.com>
2026-03-12 22:04:33 +0000

    Update CTA.js

--- themes/magzine/components/CTA.js
+++ themes/magzine/components/CTA.js
@@ -6,7 +6,7 @@ import Announcement from './Announcement'
 export default function CTA({ notice }) {
   return (
     <section
-      className="w-full bg-cyan-600 dark:bg-hexo-black-gray text-black"
+      className="w-full bg-cyan-600 dark:bg-hexo-black-gray text-white"
       aria-label="site announcement"
       style={{ paddingTop: '1rem', paddingBottom: '1rem' }} // 更紧凑的 py
     >
@@ -18,7 +18,7 @@ export default function CTA({ notice }) {
           letterSpacing: '-0.3px'
         }}
       >
-        <Announcement post={notice} className="text-center text-grey" />
+        <Announcement post={notice} className="text-center text-white" />
       </div>
     </section>
   )
