f8a22817dacede64bba2807c1123c93a54b9cd2c
TidePress <144722416+TidePress@users.noreply.github.com>
2026-06-27 17:49:54 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,6 +16,8 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
+  CUSTOM_RIGHT_CLICK_CONTEXT_MENU: process.env.NEXT_PUBLIC_CUSTOM_RIGHT_CLICK_CONTEXT_MENU || false, // 自定义右键菜单，覆盖系统菜单
+
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Tide Press', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Pursuing the noble and endless answer', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tidepress.top', // 网站地址
