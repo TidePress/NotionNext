1df5ba22596eee561f5dd5f61f0558f5b5e87823
TidePress <144722416+TidePress@users.noreply.github.com>
2026-06-27 16:34:55 +0100

    Update BannerItem.js

--- themes/magzine/components/BannerItem.js
+++ themes/magzine/components/BannerItem.js
@@ -22,12 +22,12 @@ export default function BannerItem() {
   }
 
   return (
-    <div className='flex flex-col p-5 gap-y-5 dark items-center justify-between w-full bg-black text-white'>
+    <div className='flex flex-col p-5 gap-y-5 dark items-center justify-between w-full bg-cyan-600 text-white'>
       {/* 首屏导航按钮 */}
       <h2 className='text-2xl font-semibold'>{title}</h2>
       <h3 className='text-sm'>{description}</h3>
       {button && (
-        <div className='mt-2 text-center px-6 py-3 font-semibold rounded-3xl text-black bg-[#7BE986] hover:bg-[#62BA6B]'>
+        <div className='mt-2 text-center px-6 py-3 font-semibold rounded-3xl text-black bg-cyan-500 hover:bg-cyan-800'>
           <SmartLink href={url}>{text}</SmartLink>
         </div>
       )}
