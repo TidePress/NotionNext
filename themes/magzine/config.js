3640fb77763df7d5e281067f9fff11990a4830c2
TidePress <144722416+TidePress@users.noreply.github.com>
2026-06-27 16:30:45 +0100

    Update config.js

--- themes/magzine/config.js
+++ themes/magzine/config.js
@@ -2,20 +2,20 @@ const CONFIG = {
   // 首屏信息栏按钮文字
   MAGZINE_HOME_BANNER_ENABLE: true, // 首屏右上角的宣传位
   MAGZINE_HOME_BUTTON: true,
-  MAGZINE_HOME_BUTTON_URL: '/about',
-  MAGZINE_HOME_BUTTON_TEXT: '了解更多',
+  MAGZINE_HOME_BUTTON_URL: 'mailto://tidewavereport@gmail.com',
+  MAGZINE_HOME_BUTTON_TEXT: '投稿入口',
 
   MAGZINE_HOME_HIDDEN_CATEGORY: '分享杂文', //不希望在首页展示的文章分类，用英文逗号隔开
 
-  MAGZINE_HOME_TITLE: '立即开创您的在线业务。完全免费。',
+  MAGZINE_HOME_TITLE: '想要毫无拘束地自由创作并向世界展示你的文字吗？',
   MAGZINE_HOME_DESCRIPTION:
-    '借助NotionNext，获得助您开创、经营和扩展业务所需的全部工具和帮助。',
-  MAGZINE_HOME_TIPS: 'AI时代来临，这是属于超级个体的狂欢盛宴！',
+    'Tide Press，让世界看到你的想法',
+  MAGZINE_HOME_TIPS: '',
 
   MAGZINE_HERO_SUB_POST_COUNT: 2, // 首屏英雄区次要文章数量，通常2篇，如果关闭Banner，推荐改为3篇
 
   // 首页底部推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  MAGZINE_RECOMMEND_POST_TAG: '推荐',
+  MAGZINE_RECOMMEND_POST_TAG: 'hot',
   MAGZINE_RECOMMEND_POST_COUNT: 6,
   MAGZINE_RECOMMEND_POST_TITLE: '推荐文章',
   MAGZINE_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
@@ -24,7 +24,7 @@ const CONFIG = {
   MAGZINE_RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MAGZINE_RIGHT_DARK || false, // 右侧面板深色模式
 
   MAGZINE_POST_LIST_COVER: true, // 文章列表显示图片封面
-  MAGZINE_POST_LIST_PREVIEW: false, // 列表显示文章预览
+  MAGZINE_POST_LIST_PREVIEW: true, // 列表显示文章预览
   MAGZINE_POST_LIST_CATEGORY: true, // 列表显示文章分类
   MAGZINE_POST_LIST_TAG: true, // 列表显示文章标签
 
@@ -33,13 +33,13 @@ const CONFIG = {
 
   // 文章页面联系卡
   MAGZINE_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  MAGZINE_SOCIAL_CARD_TITLE_1: '交流频道',
-  MAGZINE_SOCIAL_CARD_TITLE_2: '加入社群讨论分享',
-  MAGZINE_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  MAGZINE_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/chat-community',
+  MAGZINE_SOCIAL_CARD_TITLE_1: '投稿入口',
+  MAGZINE_SOCIAL_CARD_TITLE_2: 'Submit your works',
+  MAGZINE_SOCIAL_CARD_TITLE_3: '点击这里/Click here',
+  MAGZINE_SOCIAL_CARD_URL: 'mailto://tidewavereport@gmail.com',
 
   // 页脚菜单
-  MAGZINE_FOOTER_LINKS: [
+  /*MAGZINE_FOOTER_LINKS: [
     {
       name: '友情链接',
       menus: [
@@ -104,7 +104,7 @@ const CONFIG = {
         { title: 'NotionNext', href: 'https://docs.tangly1024.com/about' }
       ]
     }
-  ],
+  ],*/
 
   // 旧版本顶部菜单
   MAGZINE_MENU_CATEGORY: true, // 显示分类
